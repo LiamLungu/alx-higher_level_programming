13070457a83e1186ccabf379e9a021c8e5c84cc0
Liam Lungu <Dragoneyeyt2020@gmail.com>
2024-04-15 11:11:21 -0700

    Updated2

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 """class instance checker documentation"""
 
-
 def is_same_class(obj, a_class):
     """checks if the obj is exactly an instance of the specified class
-
     Return:
         True if the object is an instance, otherwise, False"""
     # This function checks if the object is the exact instance of the specified class
