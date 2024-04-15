0c8d823124af81471ca79934579d5501e50349c6
Liam Lungu <Dragoneyeyt2020@gmail.com>
2024-04-15 15:09:31 -0700

    UpdatedV1

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 """Defines a function that adds attributes to objects."""
 
 
@@ -15,24 +14,3 @@ def add_attribute(obj, att, value):
     if not hasattr(obj, "__dict__"):
         raise TypeError("can't add new attribute")
     setattr(obj, att, value)
-
-
-0x0A-python-inheritance/11-square.py
-
-#!/usr/bin/python3
-
-"""Defines a Rectangle subclass Square."""
-Rectangle = __import__('9-rectangle').Rectangle
-
-
-class Square(Rectangle):
-    """Represent a square."""
-
-    def __init__(self, size):
-        """Initialize a new square.
-        Args:
-            size (int): The size of the new square.
-        """
-        self.integer_validator("size", size)
-        super().__init__(size, size)
-        self.__size = size
